c721ed442e0cb4d11882d2d6497df9f98148688e
Kira Corbin <madkatkeys@gmail.com>
2019-06-07 23:12:51 -0700

    added values to forms

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,4 +1,5 @@
-// var score = 0;
+
+
 // for(var i=0; i < questions.length; i++){
 //     var response = window.prompt(questions[i].prompt)
 //     if(response == questions[i].answer){
@@ -7,27 +8,25 @@
 // }
 
 // alert("you got " + score + "/" + questions.length);
-// var response will access the questions if they are written out as prompts
-// [i] acts as the loop
 
-// var questions = [
-// {
-// },
-// ]
-  // alert('hello world')
+
+
+// alert('hello world')
 
 //   checking answers 
+// $("").click
 function check() {
-    const question1 = document.quiz.question1.value;
-    const question2 = document.quiz.question2.value;
-    const question3 = document.quiz.question3.value;
-    const question4 = document.quiz.question4.value;
-    const question5 = document.quiz.question5.value;
-    const question6 = document.quiz.question6.value;
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
+    var question6 = document.quiz.question6.value;
     var correct = 0;
 
     if (question1 === "Sponge Bob Square Pants") {
         correct = correct + 1
+        console.log(question1)
     }
     if (question2 === "Hawkins") {
         correct = correct + 1
@@ -44,7 +43,14 @@ function check() {
     if (question6 === "Carebears") {
         correct = correct + 1
     }
-   
-  document.getElementById("completed_questions").style.visibility = "visible";
-  document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+    var score;
+    if 
+
+// alert("you got " + score + "/" + questions.length);
+
+    document.getElementById("completed_questions").style.visibility = "visible";
+
+    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+
+
 }
