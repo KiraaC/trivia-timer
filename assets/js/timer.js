4f288c79abf82128a29647b1b496bb484152872d
Kira Corbin <madkatkeys@gmail.com>
2019-06-10 21:15:44 -0700

    my questions score works

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,20 +1,56 @@
-
-
-// for(var i=0; i < questions.length; i++){
-//     var response = window.prompt(questions[i].prompt)
-//     if(response == questions[i].answer){
-//         score++;
-//     }
+// timer 1
+// var number = 45000;
+// var intervalId;
+// function run() {
+//     clearInterval(intervalId);
+//     intervalId = setInterval(decrement, 1000);
 // }
+// function decrement() {
+//     number--;
+// }
+// if (number === 0) {
+//     stop();
+//     clearInterval(intervalId);
+// } run();
 
-// alert("you got " + score + "/" + questions.length);
+// const timerBar = document.getElementById("timer-text");
+
+// timer 2
+// function timer(seconds) {
+//     setInterval(function () {
+//         seconds--
+//     }, 45000
+    
+
+// timer 3
+//         counter++;
+//     }
+//     setInterval(timerBar, 1000)
+// }
+//     console.log('Hello')
+// function myFunction() {
+
+//     }
+//   window.setTimeout(function () {
+//         console.log('test')
 
 
+// function reset() {
+//     clock = 0;
+//     render();
 
-// alert('hello world')
+
+// var interval = setTimeout(function () {
+//     console.log
+// }     45000
+
+
+// clearTimeout()
 
 //   checking answers 
 // $("").click
+// $("#check").on("click", function (event) {
+$("#after_submit").hide();
 function check() {
     var question1 = document.quiz.question1.value;
     var question2 = document.quiz.question2.value;
@@ -29,28 +65,43 @@ function check() {
         console.log(question1)
     }
     if (question2 === "Hawkins") {
-        correct = correct + 1
+        correct = correct + 1;
     }
     if (question3 === "Mouse") {
-        correct = correct + 1
+        correct = correct + 1;
     }
     if (question4 === "Arya Stark") {
-        correct = correct + 1
+        correct = correct + 1;
     }
     if (question5 === "Balto") {
-        correct = correct + 1
+        correct = correct + 1;
     }
     if (question6 === "Carebears") {
-        correct = correct + 1
+        correct = correct + 1;
     }
-    var score;
-    if 
 
-// alert("you got " + score + "/" + questions.length);
 
-    document.getElementById("completed_questions").style.visibility = "visible";
+    // modal on bootstrap
+    $("#quiz").hide();
+    $("#after_submit").show();
+    $("#message").text("you got " + correct + "/" + 6)
+    // document.getElementById("completed_questions").style.visibility = "visible";
 
-    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+    // document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 
 
 }
+
+// }
+// for(var i=0; i < questions.length; i++){
+//     var response = window.prompt(questions[i].prompt)
+//     if(response == questions[i].answer){
+//         score++;
+//     }
+// }
+
+// alert("you got " + score + "/" + questions.length);
+
+
+
+// alert('hello world')
